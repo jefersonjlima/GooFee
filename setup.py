8f4ccd1bdf2349d06d7db858368211b66c93d60a
Jeferson Lima <jeferson@keemail.me>
2024-02-21 15:16:31 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open('requirements.txt', 'rt') as f:
     requirements_list = [req[:-1] for req in f.readlines()]
 
 setup(
-    name='quore',
+    name='GooFee',
     version='0.2.3',
-    packages=['quore', 'quore.core', 'quore.models', 'quore.filters', 'quore.segments'],
-    url='https://gitlab.com/quoretech/ecg-quore',
+    packages=['goofee', 'goofee.core', 'goofee.action', 'goofee.events'],
+    url='https://gitlab.com/jeferson.lima/GooFee',
     license='',
     author='Jeferson Lima',
-    author_email='jefersonjl82@gmail.com',
-    description='ECG-Quore: Automatic Arrhythmia Classification',
+    author_email='jeferson@keemail.me',
+    description='A simple library to show the google calendar feed on polybar bar.',
     install_requires = requirements_list)
